7ef891362d41a78366c8a44a0013ac9286e2ce07
CrazyWorldPL <kamil02167@gmail.com>
2021-05-13 16:33:10 +0200

    fix

--- zad04/main.cpp
+++ zad04/main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
-long long l1,l2,suma;
+long double l1,l2,suma;
 int main() {
     cout << "Podaj liczbe nr 1: ";
     cin >> l1;
     cout << "Podaj liczbe nr 2: ";
     cin >> l2;
     suma = l1 + l2;
-    cout << "Suma obu liczb: " << suma << endl;
+    cout << "Suma obu liczb: " << setprecision(10) << suma << endl;
     return 0;
 }
