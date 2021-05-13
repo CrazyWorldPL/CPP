0fff36700dce99b7917ed4a13382eeb72cd4ed8c
CrazyWorldPL <kamil02167@gmail.com>
2021-05-13 16:23:14 +0200

    zad02

---
+++ zad02/cmake-build-debug/CMakeFiles/zad02.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zad02.dir/main.cpp.o"
+  "zad02"
+  "zad02.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zad02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ zad02/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+long long x,y;
+int main() {
+    cout << "Podaj wartosc x: ";
+    cin >> x;
+    y = 2 * x + 2;
+    cout << "Wartosc funkcji y = 2x + 2 jest rowna: " << y << endl;
+    return 0;
+}
