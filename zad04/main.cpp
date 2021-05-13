94cc29c33e4e37930b87c77d0dd349d8511335be
CrazyWorldPL <kamil02167@gmail.com>
2021-05-13 16:23:20 +0200

    zad04

---
+++ zad04/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+long long l1,l2,suma;
+int main() {
+    cout << "Podaj liczbe nr 1: ";
+    cin >> l1;
+    cout << "Podaj liczbe nr 2: ";
+    cin >> l2;
+    suma = l1 + l2;
+    cout << "Suma obu liczb: " << suma << endl;
+    return 0;
+}
