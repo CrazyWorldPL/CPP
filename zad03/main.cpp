652d9b91c61750f582201cecbdf67ceed4f1d816
CrazyWorldPL <kamil02167@gmail.com>
2021-05-13 16:36:19 +0200

    fix

--- zad03/main.cpp
+++ zad03/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
-long long metry,milimetry;
+long double metry,milimetry;
 int main() {
     cout << "Podaj ilosc metrow: ";
     cin >> metry;
     milimetry = metry * 1000;
-    cout << "Ilosc milimetrow: " << milimetry << endl;
+    cout << "Ilosc milimetrow: " << setprecision(10) << fixed << showpoint << milimetry << endl;
     return 0;
 }
